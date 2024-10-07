13f46fd3d9429110b45aa5551ecaa97b069609d0
Shubh <shubh2268@gmail.com>
2024-10-07 16:45:06 +0530

    --

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,7 @@ import arrow_btn from '../../assets/arrow_btn.png'
 import play_icon from '../../assets/play_icon.png'
 import pause_icon from '../../assets/pause_icon.png'
 
-const Hero = ({ setPlaystatus, heroData, heroCount, setHeroCount, playstatus }) => {
+const Hero = ({ playstatus, setPlaystatus, heroData, heroCount, setHeroCount }) => {
   return (
     <div className="hero">
       <div className="hero-text">
